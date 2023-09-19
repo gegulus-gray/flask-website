d07c786b637208676b798cbe7f337ac2b6774b86
XuDa An <tudatan1708@gmail.com>
2023-09-20 01:41:00 +0700

    Add function: Create, edit, delete

--- init_db.py
+++ init_db.py
@@ -8,11 +8,11 @@ with open('schema.sql') as f:
 cur = connection.cursor()
 
 cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('First Post', 'Content for the first post')
+            ('Hula by Jasmin Iolani Hakes', 'Hula is not only one of the best beach read of 2023, but it’s also one of the best books of 2023! I picked up this book based on the cover and the thoughts of the hula dance competition, but instead of the fun read I was expecting, I got a coming-of-age story about a light-skinned Hawaiian who is trying to prove she belongs by learning to Hula.')
             )
 
 cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('Second Post', 'Content for the second post')
+            ('Trees of the Emerald Sea: A Cosmere Novel by Brandon Sanderon', 'If you love fantasy books of any kind, stop now and make time to read this fantastic book. This is the book that you would get if The Princess Bride had a book baby that empowered women everywhere to be the heroes in their own story, seek out adventure, and be brave enough to become who they were intended to be.')
             )
 
 connection.commit()
